dc72e46dd12f69e5bbbdcf5d1d1c75cb8807bbeb
gabriel <gabrielsvnzrd@gmail.com>
2021-05-09 14:18:50 -0300

    songlist renderisation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,23 @@
+import {combineReducers} from 'redux';
 
-const songsReducer = () => {
+export const songsReducer = () => {
   return [
     {title: 'Back In Black', duration: ' 3:00'},
     {title: 'Behind the Wall of Sleep', duration: '3:23'},
     {title: 'Besser du Renst', duration: ' 4:00'},
-    {title: 'Molly Chambers', duratio: '2:30'}
+    {title: 'Molly Chambers', duration: '2:30'}
   ];
 };
 
-const selectedSong = (selectedSong = null, action) => {
+export const selectedSong = (selectedSong = null, action) => {
   if (selectedSong === 'SONG_SELECTED') {
     return action.payload;
   };
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSong
+});
